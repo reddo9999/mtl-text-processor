2f5e801a2d042a4891296b953c586cb449ff8e73
reddo9999 <26098031+reddo9999@users.noreply.github.com>
2022-04-17 14:03:12 -0300

    Trim tests

---
+++ src/___tests___/Trimming.test.ts
@@ -0,0 +1,63 @@
+import { TextProcessor } from '../TextProcessor';
+
+test('Trim whole sentence and trim lines', () => {
+	let processor = new TextProcessor({
+		aggressiveSplittingPatterns: [],
+		isolateSymbolsPatterns: [],
+		lineBreakPatterns: [],
+		protectCornersPatterns: [],
+		protectedPatterns: [],
+        trim : true,
+        trimLines : true,
+        recoverPadding : false,
+	});
+	let originalSentences = ['     I love pineapples!       ', 'I love pineapples\n on my pizza!'];
+
+	let process = processor.process(...originalSentences);
+
+	let translatableStrings = process.getTranslatableLines();
+
+    expect(translatableStrings).toMatchObject([
+        "I love pineapples!",
+        "I love pineapples\non my pizza!"
+    ]);
+
+    process.setTranslatedLines(...translatableStrings);
+
+    // They are trimmed forever
+    expect(process.getTranslatedLines()).toMatchObject([
+        "I love pineapples!",
+        "I love pineapples\non my pizza!"
+    ])
+});
+
+test('Trims but recover pad', () => {
+	let processor = new TextProcessor({
+		aggressiveSplittingPatterns: [],
+		isolateSymbolsPatterns: [],
+		lineBreakPatterns: [],
+		protectCornersPatterns: [],
+		protectedPatterns: [],
+        trim : true,
+        trimLines : true,
+        recoverPadding : true
+	});
+	let originalSentences = ['     I love pineapples!       ', 'I love pineapples\n on my pizza!'];
+
+	let process = processor.process(...originalSentences);
+
+	let translatableStrings = process.getTranslatableLines();
+
+    expect(translatableStrings).toMatchObject([
+        "I love pineapples!",
+        "I love pineapples\non my pizza!"
+    ]);
+
+    process.setTranslatedLines(...translatableStrings);
+
+    // They are trimmed forever
+    expect(process.getTranslatedLines()).toMatchObject([
+        "     I love pineapples!",
+        "I love pineapples\n on my pizza!"
+    ])
+});
